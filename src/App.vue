2c27d0426883526d8075d485e2836e3b8c33a905
GMaiolo <guille.maiolo@gmail.com>
2018-09-25 02:58:52 -0300

    :rocket:

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+html,
+body,
+#app {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <div class="bg"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+  height: 100%;
+  width: 100%;
+  background-color: #fde500; // TODO: create variables file
+}
+.bg {
+  height: 100%;
+  width: 100%;
+  max-width: 768px;
+  margin: auto;
+  background: url('../assets/mtrack_promo.svg') center no-repeat;
+}
+
+</style>
