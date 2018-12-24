b7c10813a8166b90e84bcaf3ef6c640e32aa1da2
GMaiolo <guille.maiolo@gmail.com>
2018-12-24 18:03:20 -0300

    gettin it nice

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -1,12 +1,18 @@
 <template>
   <v-card>
     <v-container fluid class="pa-0">
-      <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg" aspect-ratio="2.75" ></v-img>
+      <v-img :src="url" aspect-ratio="1.25" :lazy-src="lazyUrl">
+        <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
+          <v-progress-circular indeterminate color="grey lighten-1"></v-progress-circular>
+        </v-layout>
+      </v-img>
       <v-card-title primary-title>
-        <div>
+        <v-flex xs12>
           <h4 class="title mb-0">{{ article.title }}</h4>
-          <div>Test</div>
-        </div>
+        </v-flex>
+        <v-flex xs12>
+          <div class="">$ {{ price }}</div>
+        </v-flex>
       </v-card-title>
       <v-card-actions>
         <v-btn flat color="primary">
@@ -22,11 +28,25 @@
 <script>
 export default {
   name: 'article-card',
-  props: [ 'article' ]
+  props: [ 'article' ],
+  computed: {
+    price () {
+      return this.article.history[this.article.history.length - 1].price
+    },
+    url () {
+      return this.article.images[0] || ''
+    },
+    lazyUrl () {
+      return this.url.replace('-O.jpg', '-I.jpg')
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.title {
+  min-height: 60px;
+}
 .v-badge__badge {
   position: unset;
   display: inline-block;
--- src/views/Dash.vue
+++ src/views/Dash.vue
@@ -1,31 +1,31 @@
 <template>
   <div>
-    <v-navigation-drawer v-model="drawer" fixed app>
-      <v-list dense>
-        <v-list-tile @click="1">
-          <v-list-tile-action>
-            <v-icon>home</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Home</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
     <v-toolbar app fixed clipped-left>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>MercadoTrack</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-flex xs12 sm6 md3>
-        <v-text-field solo label="Buscar" append-outer-icon="search" clearable @click:append-outer="search"></v-text-field>
-      </v-flex>
     </v-toolbar>
     <v-content>
       <div class="cards-container-wrapper">
-        <v-container fluid>
+        <v-container>
+          <v-layout>
+            <v-flex mb-3 pa-2 xs12 sm6 md4 xl3>
+              <v-text-field solo hide-details label="Buscar" append-icon="search" clearable @click:append="search"></v-text-field>
+            </v-flex>
+          </v-layout>
           <v-layout wrap>
-            <v-flex xs12 sm6 md4 xl2 pa-2 v-for="article in articles" :key="article.id">
-              <ArticleCard :article="article" />
+            <template v-if="articles">
+              <v-flex xs12 sm6 md4 xl3 pa-2 v-for="article in articles" :key="article.id">
+                <ArticleCard :article="article" />
+              </v-flex>
+            </template>
+            <template v-else-if="searching">
+              <v-flex xs12 d-flex class="o-hidden">
+                <v-progress-circular color="primary" indeterminate height="2"></v-progress-circular>
+              </v-flex>
+            </template>
+            <v-flex xs12 mt-2>
+              <v-layout justify-center>
+                <v-pagination :length="totalPages" total-visible="6" v-model="page" :disabled="searching" @input="paginate"></v-pagination>
+              </v-layout>
             </v-flex>
           </v-layout>
         </v-container>
@@ -34,29 +34,49 @@
   </div>
 </template>
 <script>
+import axios from 'axios'
 import ArticleCard from '../components/ArticleCard.vue'
+
+const limit = 25
 
 export default {
   name: 'dash',
   components: { ArticleCard },
   data: () => ({
-    drawer: false,
-    articles: []
+    articles: null,
+    searching: false,
+    page: 1,
+    totalPages: 0
   }),
   methods: {
     search () {
-      fetch(`https://api.mercadotrack.com/articles`)
-        .then(res => res.json())
-        .then(res => {
-          this.articles = res.page
-        })
+      this.page = 1
+      this.totalPages = 0
+      this.paginate()
+    },
+    paginate (pageNumber = 1) {
+      if (this.searching) return
+      this.searching = true
+      this.articles = null
+      axios.get(`https://api.mercadotrack.com/articles`, {
+        params: { limit, skip: (pageNumber - 1) * limit }
+      }).then(({ data }) => {
+        this.searching = false
+        this.articles = data.page
+        this.totalPages = ~~(data.total / limit)
+      })
     }
   }
 }
 </script>
 
-<style lang="sass" scoped>
-.cards-container-wrapper
-  height: calc(100vh - 100px)
-  overflow: auto
+<style lang="scss" scoped>
+.cards-container-wrapper {
+  height: calc(100vh - 100px);
+  overflow: auto;
+}
+
+.o-hidden {
+  overflow: hidden;
+}
 </style>
