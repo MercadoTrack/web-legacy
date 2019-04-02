094564804a8bd6f784bf901c71948ddecb8f7bd6
julietaflux <julietaflux@gmail.com>
2019-04-02 20:50:37 -0300

    working on chart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,5 +1,5 @@
 <template>
-  <canvas id="myChart" width="400" height="400"></canvas>
+  <canvas id="myChart" width="200" height="200"></canvas>
 </template>
 
 <script>
@@ -12,36 +12,41 @@ export default {
   mounted () {
     const ctx = document.getElementById('myChart').getContext('2d')
     this.chart = new Chart(ctx, {
-      type: 'bar',
+      type: 'line',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['10/03', '20/03', '25/03', '28/03', '30/03'],
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
+          label: 'Precio',
+          data: [ 3.900, 3.950, 4.200, 4.500, 4.550 ],
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 99, 132, 0.2)'
           ],
           borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 99, 132, 0.2)'
           ],
-          borderWidth: 1
+          borderWidth: 2
         }]
       },
       options: {
+        elements: {
+          line: {
+            tension: 0
+          }
+        },
         scales: {
           yAxes: [{
             ticks: {
-              beginAtZero: true
+              min: 3.000,
+              stepSize: 1.000,
+              beginAtZero: false,
             }
           }]
         }
