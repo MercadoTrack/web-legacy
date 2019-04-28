74cfd8eef095fd43b40bca517dd4d3f0f4973f90
GMaiolo <guille.maiolo@gmail.com>
2019-04-28 17:43:27 -0300

    not dense

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -6,13 +6,13 @@
       width="250"
       clipped disable-resize-watcher disable-route-watcher right app
     >
-      <v-list dense>
+      <v-list>
         <v-list-tile @click="() => {}">
           <v-list-tile-action>
             <v-icon color="orange darken-2">whatshot</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Ofertas</v-list-tile-title>
+            <v-list-tile-title class="subheading font-weight-light text-capitalize">Ofertas</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
         <v-list-tile @click="() => {}">
@@ -20,7 +20,7 @@
             <v-icon color="cyan darken-4">verified_user</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Vendedores destacados</v-list-tile-title>
+            <v-list-tile-title class="subheading font-weight-light text-capitalize">Vendedores destacados</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
         <v-list-tile @click="() => {}">
@@ -28,7 +28,7 @@
             <v-icon color="primary">sync</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Sincronizacion en vivo</v-list-tile-title>
+            <v-list-tile-title class="subheading font-weight-light text-capitalize">Sincronizacion en vivo</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
         <v-list-tile @click="() => {}">
@@ -36,7 +36,7 @@
             <v-icon color="red darken-2">favorite</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Favoritos</v-list-tile-title>
+            <v-list-tile-title class="subheading font-weight-light text-capitalize">Favoritos</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
         <v-list-tile @click="() => {}">
@@ -44,7 +44,7 @@
             <v-icon color="indigo darken-2">person</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Mi cuenta</v-list-tile-title>
+            <v-list-tile-title class="subheading font-weight-light text-capitalize">Mi cuenta</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
