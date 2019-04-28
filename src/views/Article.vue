253f161a7238ddc8e28d6689fb54e4f585585e9a
julietaflux <julietaflux@gmail.com>
2019-04-28 17:09:09 -0300

    + article view

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,6 +1,6 @@
 <template>
   <v-content>
-    <v-container grid-list-md text-xs-left>
+    <v-container text-xs-left>
       <v-progress-linear v-if="loading" :indeterminate="true"></v-progress-linear>
 
       <v-card v-else>
@@ -10,151 +10,156 @@
             <v-divider></v-divider>
           </v-flex>
 
-          <v-flex class="pa-0">
-            <section
-              v-for="url in article.images.slice(1,4)"
-              :key="url"
-              xs2 class="br pa-3"
-            >
-              <v-img
-                :src="url">
-              </v-img>
-            </section>
-          </v-flex>
+          <v-container class="pa-0 ma-0 line-wrap">
+            <v-layout wrap class="pa-0 ma-0">
+              <v-flex xs12 md2 lg2 xl2 v-if="multipleImages && $vuetify.breakpoint.mdAndUp" class="pa-0">
+                <section
+                  v-for="url in article.images.slice(1,4)"
+                  :key="url"
+                  class="pa-0"
+                >
+                  <v-img
+                    :src="url">
+                  </v-img>
+                </section>
+              </v-flex>
 
-          <v-flex xs6 class="br">
-            <v-img class="mx-5" :src="article.images[0]"></v-img>
-          </v-flex>
+              <v-layout class="pa-0 ma-0 border" align-center justify-center>
+                <v-flex xs12 md6 lg6 xl6>
+                  <v-img class="mx-0" :src="article.images[0]"></v-img>
+                </v-flex>
+              </v-layout>
 
-          <v-flex xs4 pa-4>
-            <v-list two-line subheader>
-              <span class="display-1">{{ article.title }}</span>
+              <v-flex xs12 md4 lg4 xl4 pa-4>
+                <v-list two-line subheader>
+                  <span class="display-1">{{ article.title }}</span>
 
-              <v-card flat>
-                <v-card-text class="pl-0">
-                  <p class="display-1 font-weight-light">${{ article.price }}</p>
+                  <v-card flat>
+                    <v-card-text class="pl-0">
+                      <p class="display-1 font-weight-light">${{ price }}</p>
 
-                  <v-rating v-model="rating"></v-rating>
+                      <v-rating v-model="rating"></v-rating>
 
-                  <v-list class="pt-4">
-                    <v-list-tile>
-                      <v-list-tile-action>
-                        <v-icon>payment</v-icon>
-                      </v-list-tile-action>
-                      <v-list-tile-content>
-                        <v-list-tile-title>Pagá hasta en 12 cuotas</v-list-tile-title>
-                        <v-list-tile-sub-title>Más información</v-list-tile-sub-title>
-                      </v-list-tile-content>
-                    </v-list-tile>
+                      <v-list class="pt-4">
+                        <v-list-tile>
+                          <v-list-tile-action>
+                            <v-icon>payment</v-icon>
+                          </v-list-tile-action>
+                          <v-list-tile-content>
+                            <v-list-tile-title>Pagá hasta en 12 cuotas</v-list-tile-title>
+                            <v-list-tile-sub-title>Más información</v-list-tile-sub-title>
+                          </v-list-tile-content>
+                        </v-list-tile>
 
-                    <v-list-tile>
-                      <v-list-tile-action>
-                        <v-icon>local_shipping</v-icon>
-                      </v-list-tile-action>
-                      <v-list-tile-content>
-                        <v-list-tile-title>Envío gratis</v-list-tile-title>
-                        <v-list-tile-sub-title>Llega el miércoles 10 de abril</v-list-tile-sub-title>
-                      </v-list-tile-content>
-                    </v-list-tile>
+                        <v-list-tile>
+                          <v-list-tile-action>
+                            <v-icon>local_shipping</v-icon>
+                          </v-list-tile-action>
+                          <v-list-tile-content>
+                            <v-list-tile-title>Envío gratis</v-list-tile-title>
+                            <v-list-tile-sub-title>Llega el miércoles 10 de abril</v-list-tile-sub-title>
+                          </v-list-tile-content>
+                        </v-list-tile>
 
-                    <v-list-tile>
-                      <v-list-tile-action>
-                        <v-icon>360</v-icon>
-                      </v-list-tile-action>
-                      <v-list-tile-content>
-                        <v-list-tile-title>Devolución gratis</v-list-tile-title>
-                        <v-list-tile-sub-title>Tenés 15 días desde que lo recibís</v-list-tile-sub-title>
-                      </v-list-tile-content>
-                    </v-list-tile>
+                        <v-list-tile>
+                          <v-list-tile-action>
+                            <v-icon>360</v-icon>
+                          </v-list-tile-action>
+                          <v-list-tile-content>
+                            <v-list-tile-title>Devolución gratis</v-list-tile-title>
+                            <v-list-tile-sub-title>Tenés 15 días desde que lo recibís</v-list-tile-sub-title>
+                          </v-list-tile-content>
+                        </v-list-tile>
 
-                    <v-list-tile class="pointer">
-                      <v-list-tile-action>
-                        <v-icon>call_made</v-icon>
-                      </v-list-tile-action>
-                      <v-list-tile-content>
-                        <v-list-tile-title @click="goToMLArticle">Ver en MercadoLibre</v-list-tile-title>
-                      </v-list-tile-content>
-                    </v-list-tile>
-                  </v-list>
-                </v-card-text>
-              </v-card>
-            </v-list>
-          </v-flex>
+                        <v-list-tile class="pointer">
+                          <v-list-tile-action>
+                            <v-icon>call_made</v-icon>
+                          </v-list-tile-action>
+                          <v-list-tile-content>
+                            <v-list-tile-title @click="goToMLArticle">Ver en MercadoLibre</v-list-tile-title>
+                          </v-list-tile-content>
+                        </v-list-tile>
+                      </v-list>
+                    </v-card-text>
+                  </v-card>
+                </v-list>
+              </v-flex>
+            </v-layout>
+          </v-container>
+              <v-flex xs12 py-0>
+                <v-divider></v-divider>
+              </v-flex>
 
-          <v-flex xs12 py-0>
-            <v-divider></v-divider>
-          </v-flex>
+              <v-flex xs12 md8 lg8 xl8 pa-4 class="border">
+                <Chart :history="article.history" />
+              </v-flex>
 
-          <v-flex xs8 pa-4 class="br">
-            <Chart :history="article.history" />
-          </v-flex>
+              <v-flex xs12 md4 lg4 xl4 pa-4>
+                <v-list two-line subheader>
+                  <v-subheader class="headline">Historial de precios</v-subheader>
+                  <v-list-tile>
+                    <v-list-tile-content>
+                      <v-tooltip right max-width="25rem">
+                        <template slot="activator">
+                          <v-list-tile-title class="title">{{ article.history.length }}</v-list-tile-title>
+                          <v-list-tile-sub-title>variaciones</v-list-tile-sub-title>
+                        </template>
+                        <span>Es la cantidad de veces que el precio publicado sufrió modificaciones.</span>
+                      </v-tooltip>
+                    </v-list-tile-content>
+                  </v-list-tile>
 
-          <v-flex xs4 pa-4>
-            <v-list two-line subheader>
-              <v-subheader class="headline">Historial de precios</v-subheader>
-              <v-list-tile>
-                <v-list-tile-content>
-                  <v-tooltip right max-width="25rem">
-                    <template slot="activator">
-                      <v-list-tile-title class="title">{{ article.history.length }}</v-list-tile-title>
-                      <v-list-tile-sub-title>variaciones</v-list-tile-sub-title>
-                    </template>
-                    <span>Es la cantidad de veces que el precio publicado sufrió modificaciones.</span>
-                  </v-tooltip>
-                </v-list-tile-content>
-              </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-content>
+                      <v-tooltip right max-width="25rem">
+                        <template slot="activator">
+                          <v-list-tile-title class="title">${{ price }}</v-list-tile-title>
+                          <v-list-tile-sub-title>Precio actual publicado</v-list-tile-sub-title>
+                        </template>
+                        <span>Es el precio publicado en MercadoLibre por el vendedor.</span>
+                    </v-tooltip>
+                    </v-list-tile-content>
+                  </v-list-tile>
 
-              <v-list-tile>
-                <v-list-tile-content>
-                  <v-tooltip right max-width="25rem">
-                    <template slot="activator">
-                      <v-list-tile-title class="title">${{ article.price }}</v-list-tile-title>
-                      <v-list-tile-sub-title>Precio actual publicado</v-list-tile-sub-title>
-                    </template>
-                    <span>Es el precio publicado en MercadoLibre por el vendedor.</span>
-                </v-tooltip>
-                </v-list-tile-content>
-              </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-content>
+                      <v-tooltip right max-width="25rem">
+                        <template slot="activator">
+                          <v-list-tile-title class="title">${{ price }}</v-list-tile-title>
+                          <v-list-tile-sub-title>Precio real</v-list-tile-sub-title>
+                          </template>
+                        <span>Es el precio sobre el cual se aplicó el descuento publicado.</span>
+                      </v-tooltip>
+                    </v-list-tile-content>
+                  </v-list-tile>
 
-              <v-list-tile>
-                <v-list-tile-content>
-                  <v-tooltip right max-width="25rem">
-                    <template slot="activator">
-                      <v-list-tile-title class="title">${{ article.price }}</v-list-tile-title>
-                      <v-list-tile-sub-title>Precio real</v-list-tile-sub-title>
-                      </template>
-                    <span>Es el precio sobre el cual se aplicó el descuento publicado.</span>
-                  </v-tooltip>
-                </v-list-tile-content>
-              </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-content>
+                      <v-tooltip right max-width="25rem">
+                        <template slot="activator">
+                          <v-list-tile-title class="title">%0</v-list-tile-title>
+                          <v-list-tile-sub-title>Descuento publicado</v-list-tile-sub-title>
+                          </template>
+                        <span>Es el descuento publicado en Mercado Libre por el vendedor</span>
+                      </v-tooltip>
+                    </v-list-tile-content>
+                  </v-list-tile>
 
-              <v-list-tile>
-                <v-list-tile-content>
-                  <v-tooltip right max-width="25rem">
-                    <template slot="activator">
-                      <v-list-tile-title class="title">%0</v-list-tile-title>
-                      <v-list-tile-sub-title>Descuento publicado</v-list-tile-sub-title>
-                      </template>
-                    <span>Es el descuento publicado en Mercado Libre por el vendedor</span>
-                  </v-tooltip>
-                </v-list-tile-content>
-              </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-content>
+                      <v-tooltip right max-width="25rem">
+                        <template slot="activator">
+                          <v-list-tile-title class="title">%0</v-list-tile-title>
+                          <v-list-tile-sub-title>Descuento real</v-list-tile-sub-title>
+                          </template>
+                        <span>Es el descuento real del producto en base a su precio anterior</span>
+                      </v-tooltip>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </v-list>
+              </v-flex>
 
-              <v-list-tile>
-                <v-list-tile-content>
-                  <v-tooltip right max-width="25rem">
-                    <template slot="activator">
-                      <v-list-tile-title class="title">%0</v-list-tile-title>
-                      <v-list-tile-sub-title>Descuento real</v-list-tile-sub-title>
-                      </template>
-                    <span>Es el descuento real del producto en base a su precio anterior</span>
-                  </v-tooltip>
-                </v-list-tile-content>
-              </v-list-tile>
-            </v-list>
-          </v-flex>
-
-          <v-flex xs12 py-0>
+              <v-flex xs12 py-0>
             <v-divider></v-divider>
           </v-flex>
 
@@ -255,6 +260,16 @@ export default {
         this.loading = false
       })
   },
+  computed: {
+    price () {
+      return this.article.history[this.article.history.length - 1].price
+    },
+    multipleImages () {
+      if (this.article.images.length > 1) {
+        return true
+      } 
+    },
+  },
   methods: {
     goToMLArticle () {
       window.open(this.article.permalink)
@@ -264,7 +279,14 @@ export default {
 </script>
 
 <style scoped>
-.br {
+.border {
   border-right: 1px solid rgba(0,0,0,.12);
+  border-left: 1px solid rgba(0,0,0,.12);
 }
+
+.line-wrap {
+  height: auto;
+  max-height: auto;
+}
+
 </style>
