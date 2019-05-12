f2490c55449ab6d7d71913a97d7bfe1c317ac8aa
GMaiolo <guille.maiolo@gmail.com>
2019-05-12 17:01:19 -0300

    changing some sizes

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -2,9 +2,9 @@
   <v-content>
     <v-container>
       <div v-for="category in categoriesWithSamples" :key="category._id" class="mb-5">
-        <h2 class="display-1 grey--text text--darken-3 font-weight-light">{{ category.name }}</h2>
-        <v-layout row wrap class="my-2">
-          <v-flex v-for="article in category.samples" :key="article.id" xs3 class="pa-2">
+        <h2 class="headline grey--text text--darken-3 font-weight-light pl-2">{{ category.name }}</h2>
+        <v-layout row class="my-2">
+          <v-flex xs3 v-for="article in category.samples" :key="article.id" class="pa-2">
             <v-card hover>
               <router-link :to="`/article/${article.id}`">
                 <v-img :src="getArticleImg(article)" contain height="200px"></v-img>
@@ -37,7 +37,7 @@ export default {
     },
     populateCategoriesWithSamples (mainCategories) {
       mainCategories.forEach((category) => {
-        http.get(`/articles?limit=8&skip=0&category=${category._id}&pretty=true`).then(res => {
+        http.get(`/articles?limit=4&skip=0&category=${category._id}&pretty=true`).then(res => {
           this.categoriesWithSamples.push({
             ...category,
             samples: res.data.page
