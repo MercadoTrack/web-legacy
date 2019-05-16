86edcbe326b9a398b117a9983ae885f800b89d02
GMaiolo <guille.maiolo@gmail.com>
2019-05-16 00:39:52 -0300

    better mobile view for landing

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,28 +1,36 @@
 <template>
   <Intro v-if="showIntro" :dismiss="dismissIntro"/>
   <v-content v-else>
-    <v-container class="mt-4">
-      <div v-for="category in mainCategories" :key="category._id" class="mb-5">
-        <template v-if="$vuetify.breakpoint.smAndDown">
-          <router-link :to="getCategoryLink(category)" class="mobile-wrapper pl-3">
-            <h3 class="subheading font-weight-light">{{ category.name }}</h3>
-            <v-icon class="ml-3" x-large>{{ getCategoryIcon(category) }}</v-icon>
-          </router-link>
-        </template>
-        <template v-else>
-          <h2 class="headline font-weight-light ml-2">
-            {{ category.name }}
-            <router-link :to="getCategoryLink(category)" class="ml-2 subheading">
-              Ver todo
+    <v-container fill-height class="mt-4">
+      <v-layout row wrap>
+        <v-flex xs6 md12 v-for="category in mainCategories" :key="category._id">
+          <template v-if="$vuetify.breakpoint.smAndDown">
+            <router-link :to="getCategoryLink(category)" class="mobile-wrapper px-3 text-xs-center">
+              <v-layout row wrap>
+                <v-flex xs12>
+                  <v-icon style="font-size: 7rem" color="primary">{{ getCategoryIcon(category) }}</v-icon>
+                </v-flex>
+                <v-flex xs12>
+                  <h3 class="subheading">{{ category.name }}</h3>
+                </v-flex>
+              </v-layout>
             </router-link>
-          </h2>
-          <v-layout row class="my-2">
-            <v-flex xs3 v-for="article in category.samples" :key="article.id" class="pa-2">
-              <LandingArticleCard :article="article" />
-            </v-flex>
-          </v-layout>
-        </template>
-      </div>
+          </template>
+          <div v-else class="mb-5">
+            <h2 class="headline font-weight-light ml-2">
+              {{ category.name }}
+              <router-link :to="getCategoryLink(category)" class="ml-2 subheading primary--text">
+                Ver todo
+              </router-link>
+            </h2>
+            <v-layout row class="my-2">
+              <v-flex xs3 v-for="article in category.samples" :key="article.id" class="pa-2">
+                <LandingArticleCard :article="article" />
+              </v-flex>
+            </v-layout>
+          </div>
+        </v-flex>
+      </v-layout>
     </v-container>
   </v-content>
 </template>
