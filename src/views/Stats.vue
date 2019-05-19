9a5ce04497eef2b85eaf25f4dfd4f0a3dd6f03cf
julietaflux <julietaflux@gmail.com>
2019-05-19 15:50:55 -0300

    + stats language

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -7,25 +7,25 @@
           <v-progress-linear color="info darken-2" height="10" :value="percentage"></v-progress-linear>
           <v-layout wrap justify-center>
             <h1 class="subheading">
-              <span class="font-weight-black">{{ processed }}</span> Articles processed of
-              <span class="font-weight-black">{{ total }}</span> total
+              <span class="font-weight-black">{{ processed }}</span> artículos procesados de 
+              <span class="font-weight-black">{{ total }}</span>
             </h1>
             <v-flex xs12>
               <v-divider class="my-4"></v-divider>
               <v-card>
-                <v-card-title><h4>Sync Progress</h4></v-card-title>
+                <v-card-title><h4>Proceso de sincronización</h4></v-card-title>
                 <v-divider></v-divider>
                 <v-list>
                   <v-list-tile>
-                    <v-list-tile-content>Completion:</v-list-tile-content>
+                    <v-list-tile-content>Total procesado:</v-list-tile-content>
                     <v-list-tile-content class="align-end text-xs-right">{{ percentage }} %</v-list-tile-content>
                   </v-list-tile>
                   <v-list-tile>
-                    <v-list-tile-content>Time Running:</v-list-tile-content>
-                    <v-list-tile-content class="align-end text-xs-right">{{ timeRunning }} minutes</v-list-tile-content>
+                    <v-list-tile-content>Tiempo corriendo:</v-list-tile-content>
+                    <v-list-tile-content class="align-end text-xs-right">{{ timeRunning }} minutos</v-list-tile-content>
                   </v-list-tile>
                   <v-list-tile>
-                    <v-list-tile-content>Estimated Time of Completion:</v-list-tile-content>
+                    <v-list-tile-content>Tiempo estimado para terminar:</v-list-tile-content>
                     <v-list-tile-content class="align-end text-xs-right">~{{ etc }}</v-list-tile-content>
                   </v-list-tile>
                   <div class="px-3" :style="{ 'margin-left': errors.length ? '-6px' : '-30px' }">
@@ -73,7 +73,7 @@ export default {
         this.errorsCount = data.progress.errorsCount
         this.errors = data.progress.errors
         this.errorsTree = [{
-          name: `Errors (${this.errors.length})`,
+          name: `Errores (${this.errors.length})`,
           children: this.errors.reduce((acc, error) => [...acc, { name: error }], [])
         }]
       })
