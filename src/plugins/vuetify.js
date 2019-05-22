9a0ba5fa7e4044ba23312a3d64db83db64fcaac9
GMaiolo <guille.maiolo@gmail.com>
2019-05-21 22:26:03 -0300

    mdi in css

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 import colors from 'vuetify/es5/util/colors'
 
