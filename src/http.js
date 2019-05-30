bf4aec9c9af27192cece40cd33994ee5eae16a7e
GMaiolo <guille.maiolo@gmail.com>
2019-05-30 01:12:48 -0300

    commented with credentials because of cors errors

--- src/http.js
+++ src/http.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const mtInstance = axios.create({
   baseURL: 'https://api.mercadotrack.com/',
   timeout: 15000,
-  withCredentials: true
+  // withCredentials: true
 })
 
 export default mtInstance
