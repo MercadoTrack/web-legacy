aa3d94e624f458f168119258a72cb214cabac62f
GMaiolo <guille.maiolo@gmail.com>
2019-06-01 04:16:05 -0300

    share layout working with to prop

--- src/components/ArticleCard/ArticleCardImage.vue
+++ src/components/ArticleCard/ArticleCardImage.vue
@@ -1,18 +1,5 @@
 <template>
   <v-img :src="imageUrl" contain aspect-ratio="1.3" :lazy-src="lazyImageUrl">
-    <v-container class="pa-3" fill-height fluid>
-      <v-layout column align-end>
-        <span v-if="hover" class="headline">
-          <v-btn fab small class="ma-0" @click="share">
-            <v-icon color="primary">share</v-icon>
-          </v-btn>
-          <v-btn fab small class="ma-0 ml-2" @click="toggleFavorite">
-            <v-icon v-if="favorite" color="primary">favorite</v-icon>
-            <v-icon v-else color="primary">favorite_border</v-icon>
-          </v-btn>
-        </span>
-      </v-layout>
-    </v-container>
     <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
       <v-progress-circular indeterminate color="grey lighten-1"></v-progress-circular>
     </v-layout>
@@ -21,11 +8,8 @@
 
 <script>
 export default {
-  name: 'article-card-image',
-  props: ['article', 'hover'],
-  data: () => ({
-    favorite: false,
-  }),
+  name: 'ArticleCardImage',
+  props: ['article'],
   computed: {
     imageUrl () {
       const fallback = 'https://http2.mlstatic.com/resources/frontend/statics/img-not-available/1.0.0/V.jpg'
@@ -35,15 +19,5 @@ export default {
       return this.imageUrl.replace('-O.jpg', '-I.jpg')
     }
   },
-  methods: {
-    share (event) {
-      event.stopPropagation()
-      this.$store.commit('share/setArticle', this.article)
-    },
-    toggleFavorite (event) {
-      event.stopPropagation()
-      this.favorite = !this.favorite
-    }
-  }
 }
 </script>
---
+++ src/components/ArticleCard/ArticleCardShare.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container fluid class="absolute" style="z-index: 1;">
+    <v-layout column align-end>
+      <span v-if="hover" class="headline">
+        <v-btn fab small class="ma-0" @click="share">
+          <v-icon color="primary">share</v-icon>
+        </v-btn>
+        <v-btn fab small class="ma-0 ml-2" @click="toggleFavorite">
+          <v-icon v-if="favorite" color="primary">favorite</v-icon>
+          <v-icon v-else color="primary">favorite_border</v-icon>
+        </v-btn>
+      </span>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'ArticleCardShare',
+  props: ['article', 'hover'],
+  data: () => ({
+    favorite: false,
+  }),
+  methods: {
+    share (event) {
+      event.stopPropagation()
+      this.$store.commit('share/setArticle', this.article)
+    },
+    toggleFavorite (event) {
+      event.stopPropagation()
+      this.favorite = !this.favorite
+    }
+  }
+}
+</script>
