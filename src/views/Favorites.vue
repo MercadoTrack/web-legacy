7305876baad092f3fa917b501897b6a4753aaf30
GMaiolo <guille.maiolo@gmail.com>
2019-06-15 03:43:56 -0300

    basic favorite articles with real favorites

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -18,13 +18,13 @@
                     </v-list-tile-action>
 
                     <v-list-tile-content>
-                      <v-list-tile-title>Todos ({{this.items.length}})</v-list-tile-title>
+                      <v-list-tile-title>Todos ({{this.articles.length}})</v-list-tile-title>
                     </v-list-tile-content>
                   </v-list-tile>
 
                   <v-list-tile
-                    v-for="item in items"
-                    :key="item.id"
+                    v-for="article in articles"
+                    :key="article.id"
                     avatar
                     class="line border"
                     >
@@ -34,19 +34,19 @@
                     </v-list-tile-action>
 
                     <v-list-tile-avatar :tile="false" size="150">
-                      <img :src="item.img">
+                      <img :src="getImage(article)">
                     </v-list-tile-avatar>
 
                     <v-list-tile-content class="ml-4 article-caption">
-                      <v-list-tile-title class="title font-weight-light mb-2">{{ item.title }}</v-list-tile-title>
-                      <v-list-tile-sub-title class="title font-weight-light">${{ item.price }}</v-list-tile-sub-title>
+                      <v-list-tile-title class="title font-weight-light mb-2">{{ article.title }}</v-list-tile-title>
+                      <v-list-tile-sub-title class="title font-weight-light">{{ article.price | priceFilter }}</v-list-tile-sub-title>
                     </v-list-tile-content>
 
                     <v-tooltip bottom max-width="25rem">
                       <template slot="activator">
                         <v-list-tile-action>
-                          <v-btn icon ripple>
-                            <v-icon color="grey lighten-1" @click="goToMLArticle(item.permalink)">call_made</v-icon>
+                          <v-btn icon ripple :href="article.permalink" target="_blank">
+                            <v-icon color="grey lighten-1">call_made</v-icon>
                           </v-btn>
                         </v-list-tile-action>
                       </template>
@@ -56,7 +56,7 @@
                     <v-tooltip bottom max-width="25rem">
                       <template slot="activator">
                         <v-list-tile-action>
-                          <v-btn icon ripple>
+                          <v-btn icon ripple :to="`/articulo/${article.id}`">
                             <v-icon color="grey lighten-1">show_chart</v-icon>
                           </v-btn>
                         </v-list-tile-action>
@@ -87,47 +87,12 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import api from '../api'
 
 export default {
   data: () => ({
     loading: true,
-    items: [
-      {
-        img: 'https://mla-s1-p.mlstatic.com/723842-MLA27773252626_072018-G.jpg',
-        title: 'Velador Decorativo Con Tulipa. Base De Madera',
-        permalink: 'https://articulo.mercadolibre.com.ar/MLA-624428536-velador-decorativo-con-tulipa-base-de-madera-moderno-luna-_JM',
-        price: '750',
-        id: 1
-      },
-      {
-        img: 'https://mla-s2-p.mlstatic.com/812373-MLA29749196213_032019-G.jpg',
-        title: 'Tv Smart Samsung 40j5200 40 Pulgadas Full Hd',
-        permalink: 'https://articulo.mercadolibre.com.ar/MLA-733113241-tv-smart-samsung-40j5200-40-pulgadas-full-hd-_JM',
-        price: '15.899',
-        id: 2
-      },
-      {
-        img: 'https://mla-s1-p.mlstatic.com/742506-MLA28126349802_092018-G.jpg',
-        title: 'Edifier M1360 Parlantes 2.1 Mute Pc Tv Control Volumen',
-        permalink: 'https://articulo.mercadolibre.com.ar/MLA-608770332-edifier-m1360-parlantes-21-mute-pc-tv-control-volumen-_JM',
-        price: '2.690',
-        id: 3
-      },
-      {
-        img: 'https://mla-s2-p.mlstatic.com/743457-MLA27722727897_072018-G.jpg',
-        title: 'Almohada Cannon Sublime X2, Envío S/c A Todo El País*',
-        permalink: 'https://articulo.mercadolibre.com.ar/MLA-736464289-almohada-cannon-sublime-x2-envio-sc-a-todo-el-pais-_JM',
-        price: '1.750',
-        id: 4
-      },
-      {
-        img: 'https://mla-s2-p.mlstatic.com/842655-MLA29650554913_032019-G.jpg',
-        title: 'Auriculares Bluetooth Soundbuds Sport Nb10',
-        permalink: 'https://articulo.mercadolibre.com.ar/MLA-776100485-auriculares-bluetooth-soundbuds-sport-nb10-_JM',
-        price: '2.500',
-        id: 5
-      }
-    ]
+    articles: []
   }),
   computed: {
     ...mapGetters({
@@ -137,18 +102,26 @@ export default {
     })
   },
   methods: {
-    goToMLArticle (link) {
-      window.open(link)
+    getImage (article) {
+      const [image] = article.images
+      return image
     }
   },
   mounted () {
-    const interval = setInterval(() => {
+    const interval = setInterval(async () => {
       if (!this.authenticating) {
         if (!this.isAuthenticated) {
           this.$router.push('/')
         }
-        this.loading = false
         clearInterval(interval)
+        // TODO: refactor this from the BE so we only do 1 paginated request
+        this.articles = await Promise.all(
+          this.favorites.map(async (id) => {
+            const { data } = await api.getArticle(id)
+            return data
+          })
+        )
+        this.loading = false
       }
     }, 100)
   }
