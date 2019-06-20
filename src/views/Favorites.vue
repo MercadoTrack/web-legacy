a5fe5674eba5a0a4ee219d8a1a3ebd5e65d43d49
GMaiolo <guille.maiolo@gmail.com>
2019-06-20 01:18:25 -0300

    select all correctly working and code polish

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -23,11 +23,12 @@
                 <v-list>
                   <v-list-tile class="line py-2">
                     <v-list-tile-action>
-                      <v-checkbox v-model="allSelected" @click.native="checkAllArticles"></v-checkbox>
+                      <v-checkbox :value="selected.length === favorites.length" @click.native="selectAll()"></v-checkbox>
                     </v-list-tile-action>
 
                     <v-list-tile-content>
-                      <v-btn :disabled="this.selected.length === 0"
+                      <v-btn
+                        :disabled="!this.selected.length"
                         @click="deleteMultiple()"
                         light color="info"
                         class="px-1 font-weight-light text-capitalize">
@@ -52,21 +53,10 @@
                       <img :src="getImage(article)">
                     </v-list-tile-avatar>
 
-                    <v-list-tile-content class="ml-4 article-caption">
+                    <v-list-tile-content class="ml-4">
                       <v-list-tile-title class="title font-weight-light mb-2">{{ article.title }}</v-list-tile-title>
                       <v-list-tile-sub-title class="title font-weight-light">{{ article.price | priceFilter }}</v-list-tile-sub-title>
                     </v-list-tile-content>
-
-                    <v-tooltip bottom max-width="25rem">
-                      <template slot="activator">
-                        <v-list-tile-action>
-                          <v-btn icon ripple :href="article.permalink" target="_blank">
-                            <v-icon color="grey lighten-1">call_made</v-icon>
-                          </v-btn>
-                        </v-list-tile-action>
-                      </template>
-                        <span>Ver en Mercado Libre</span>
-                    </v-tooltip>
 
                     <v-tooltip bottom max-width="25rem">
                       <template slot="activator">
@@ -109,7 +99,6 @@ export default {
     loading: true,
     articles: [],
     selected: [],
-    allSelected: false
   }),
   metaInfo: {
     title: 'Favoritos en MercadoTrack'
@@ -133,36 +122,33 @@ export default {
       this.articles = this.articles.filter(article => !ids.includes(article.id))
     },
     async deleteMultiple () {
-      if (this.selected.length > 0) {
-        const res = await api.removeFavorites(this.selected)
-        this.updateFavorites(res.data)
-        this.removeFromArray(this.selected)
-      }
+      const { data: favorites } = await api.removeFavorites(this.selected)
+      this.updateFavorites(favorites)
+      this.removeFromArray(this.selected)
     },
-    async fetchFavorites () {
-      this.articles = (await api.getFavorites({ full: true })).data
-    },
-    checkAllArticles () {
-      if (this.allSelected) {
-        this.selected = this.articles.map((article) => article.id)
+    selectAll () {
+      if (!this.selected.length || this.selected.length < this.favorites.length) {
+        this.selected = this.articles.map(article => article.id)
       } else {
         this.selected = []
       }
     },
     async toggleFavorite (article) {
-      const res = await api.toggleFavorite(article.id)
-      this.updateFavorites(res.data)
-      this.removeFromArray([article.id])
+      const { data: favorites } = await api.toggleFavorite(article.id)
+      this.updateFavorites(favorites)
+      this.removeFromArray([ article.id ])
     }
   },
   mounted () {
     const interval = setInterval(async () => {
       if (!this.authenticating) {
+        clearInterval(interval)
         if (!this.isAuthenticated) {
           this.$router.push('/')
+          return
         }
-        clearInterval(interval)
-        await this.fetchFavorites()
+        const { data: articles } = await api.getFavorites({ full: true })
+        this.articles = articles
         this.loading = false
       }
     }, 100)
@@ -180,7 +166,4 @@ export default {
   padding: 5rem 1rem;
 }
 
-.article-caption {
-  min-height: 50px;
-}
 </style>
