da4bf6ea1145d1820fec5139863c06dfaa9a819e
GMaiolo <guille.maiolo@gmail.com>
2019-06-20 02:02:00 -0300

    resetting selected on delete

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -118,6 +118,7 @@ export default {
     },
     async remove (ids = this.selected) {
       const { data: favorites } = await api.removeFavorites(ids)
+      this.selected = []
       this.articles = this.articles.filter(article => !ids.includes(article.id))
       this.updateFavorites(favorites)
     },
