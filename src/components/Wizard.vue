e50f37a746f0e3475440e75a4ae5eadf1c1008ea
GMaiolo <guille.maiolo@gmail.com>
2019-07-08 23:20:17 -0300

    renamed variables and polished base styles

--- src/components/Wizard.vue
+++ src/components/Wizard.vue
@@ -13,38 +13,38 @@
             <v-icon>close</v-icon>
           </v-btn>
         </v-toolbar>
-        <v-stepper v-model="e6" vertical>
-          <v-stepper-step :complete="e6 > 1" step="1">
+        <v-stepper v-model="step" vertical>
+          <v-stepper-step :complete="step > 1" step="1">
             Bienvenido a MercadoTrack
           </v-stepper-step>
 
           <v-stepper-content step="1">
-              <small>Descripcion de quienes somos y por que hacemos lo que hacemos.</small>
-            <v-btn small color="primary" @click="e6 = 2">Continuar</v-btn>
-            <v-btn small color="primary" @click="seeLater">Ver mas tarde</v-btn>
+            <p>Descripcion de quienes somos y por que hacemos lo que hacemos.</p>
+            <v-btn color="primary" @click="step = 2">Continuar</v-btn>
+            <v-btn color="primary" @click="seeLater">Ver mas tarde</v-btn>
           </v-stepper-content>
 
-          <v-stepper-step :complete="e6 > 2" step="2">Configure analytics for this app</v-stepper-step>
+          <v-stepper-step :complete="step > 2" step="2">Configure analytics for this app</v-stepper-step>
 
           <v-stepper-content step="2">
             <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-            <v-btn color="primary" @click="e6 = 3">Continuar</v-btn>
-            <v-btn flat @click="e6 = 1">Volver</v-btn>
+            <v-btn color="primary" @click="step = 3">Continuar</v-btn>
+            <v-btn flat @click="step = 1">Volver</v-btn>
           </v-stepper-content>
 
-          <v-stepper-step :complete="e6 > 3" step="3">Select an ad format and name ad unit</v-stepper-step>
+          <v-stepper-step :complete="step > 3" step="3">Select an ad format and name ad unit</v-stepper-step>
 
           <v-stepper-content step="3">
             <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-            <v-btn color="primary" @click="e6 = 4">Continuar</v-btn>
-            <v-btn flat @click="e6 = 2">Volver</v-btn>
+            <v-btn color="primary" @click="step = 4">Continuar</v-btn>
+            <v-btn flat @click="step = 2">Volver</v-btn>
           </v-stepper-content>
 
           <v-stepper-step step="4">View setup instructions</v-stepper-step>
           <v-stepper-content step="4">
             <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
             <v-btn color="primary" @click="hide">Estoy listo!</v-btn>
-            <v-btn flat @click="e6 = 3">Volver</v-btn>
+            <v-btn flat @click="step = 3">Volver</v-btn>
           </v-stepper-content>
         </v-stepper>
       </v-card>
@@ -57,7 +57,7 @@ import { mapGetters, mapMutations } from 'vuex'
 export default {
   name: 'wizard',
   data: () => ({
-    e6: null,
+    step: null,
   }),
   computed: {
     ...mapGetters({
