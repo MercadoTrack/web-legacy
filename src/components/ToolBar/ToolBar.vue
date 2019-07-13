f90e28ce5d1db54c4c0f5dd3df0596b9d56f741f
julietaflux <julietaflux@gmail.com>
2019-07-13 15:24:08 -0300

    + tooltip text

--- src/components/ToolBar/ToolBar.vue
+++ src/components/ToolBar/ToolBar.vue
@@ -57,7 +57,7 @@
           ref="searchInput"
           height="40"
           append-icon="search"
-          label="Buscar"
+          label="Buscar o pegar link"
           v-model="searchTerm"
           @keyup.enter="search()"
           @click:append="search()"
