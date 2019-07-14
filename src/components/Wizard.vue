040080ed6723ee110527c9b528b0603e0689740f
julietaflux <julietaflux@gmail.com>
2019-07-13 23:45:59 -0300

    wizard wording

--- src/components/Wizard.vue
+++ src/components/Wizard.vue
@@ -9,12 +9,12 @@
           </v-btn>
         </v-toolbar>
         <v-stepper v-model="step" vertical>
-          <v-stepper-step :complete="step > 1" step="1">Bienvenido a MercadoTrack</v-stepper-step>
+          <v-stepper-step :complete="step > 1" step="1">¡Bienvenido!</v-stepper-step>
 
           <v-stepper-content step="1">
-            <p>En nuestra plataforma vas a poder acceder, en tiempo real, a los cambios de precio de las publicaciones de MercadoLibre.</p>
+            <p>En nuestra plataforma vas a poder acceder, en tiempo real, a los cambios en los precios de las publicaciones de MercadoLibre.</p>
             <p>
-              La información del sitio esta siendo
+              La información del sitio es
               <router-link to="/stats">actualizada constantemente</router-link> para que puedas identificar cual es la mejor oferta para vos.
             </p>
             <v-btn color="primary" @click="step = 2">¿Cómo se usa?</v-btn>
@@ -24,10 +24,12 @@
           <v-stepper-step :complete="step > 2" step="2">Buscar un artículo</v-stepper-step>
 
           <v-stepper-content step="2">
-            <p>Si ya viste en MercadoLibre algo que te interesa podés pegar el link en la barra de búsqueda y presionar ´enter´.</p>
-            <p>Esto te va a llevar a la página del artículo en donde vas a encontrar su información y los precios históricos del mismo.</p>
+            <p>Podés navegar y buscar por palabra clave o navegar entre las categorías disponibles.</p>
             <v-card color="grey lighten-1" class="mb-3" height="200px"></v-card>
-            <p>Si no también podés navegar y buscar por palabra clave o categoría. En este caso te vamos a mostrar todos los resultados que coinciden con tu búsqueda para que puedas mirar el artículo que te interese.</p>
+            <p>Si te interesa un artículo de MercadoLibre en particular, también podes copiar y pegar su link en nuestra barra de búsqueda y presionar ´enter´.</p>
+            <v-card color="grey lighten-1" class="mb-3" height="200px"></v-card>
+            <p>Y esto te llevará directamente a la página del artículo en donde vas a encontrar su información y precios históricos.</p>
+            <v-card color="grey lighten-1" class="mb-3" height="200px"></v-card>
             <v-btn color="primary" @click="step = 3">Continuar</v-btn>
             <v-btn flat @click="step = 1">Volver</v-btn>
           </v-stepper-content>
@@ -35,10 +37,10 @@
           <v-stepper-step :complete="step > 3" step="3">Favoritos</v-stepper-step>
 
           <v-stepper-content step="3">
-            <p>Si querés guardar algún artículo para consultarlo más tarde podés hacer click en el corazón al lado del titulo.</p>
+            <p>Podés clickear el ícono de corazón junto al título de cada artículo para guardarlo en caso de que quieras consultarlo luego.</p>
+            <p>¡Importante! Recordá que para hacerlo tendrás que iniciar sesión.</p>
             <v-card color="grey lighten-1" class="mb-3" height="200px"></v-card>
-            <p>Pero recordá que para hacerlo vas a tener que iniciar sesión.</p>
-            <p>Si ya te registraste y guardaste un artículo, vas a poder acceder a todos ellos desde <router-link to="/favoritos" target="_blank">Mi Cuenta > Favoritos</router-link></p>
+            <p>Si ya estás registrado e iniciaste sesión, podrás acceder y editar todos tus favoritos desde <router-link to="/favoritos" target="_blank">Mi Cuenta > Favoritos</router-link></p>
             <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
             <v-btn color="primary" @click="step = 4">Continuar</v-btn>
             <v-btn flat @click="step = 2">Volver</v-btn>
