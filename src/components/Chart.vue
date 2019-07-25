4b2f0bd4206b6073d997a09daf23f5d12c360c49
GMaiolo <guille.maiolo@gmail.com>
2019-07-25 00:41:59 -0300

    inflation playground

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -6,6 +6,7 @@
 import { format } from 'date-fns'
 import Chart from 'chart.js'
 import 'chartjs-plugin-trendline'
+import _inflation from './inflation-mock.json'
 
 export default {
   data: () => ({
@@ -28,23 +29,56 @@ export default {
     const history = lastSnapshot.date !== todaySnapshot.date
       ? [...this.history, todaySnapshot]
       : this.history
+
+    let lastFoundInflation = null
+    let lastCalculatedInflationPrice = null
+    const inflation = history.map(({ date, price }) => {
+      const [ , histMonth, histYear ] = date.split('/')
+      const inflationInDate = _inflation.find(({ d }) => {
+        const [ inflYear, inflMonth ] = d.split('-')
+        return inflYear === histYear && histMonth === inflMonth
+      })
+      if (!inflationInDate) return
+      if (!lastFoundInflation) {
+        lastFoundInflation = inflationInDate
+        lastCalculatedInflationPrice = price
+        return price
+      }
+      if (lastFoundInflation.d === inflationInDate.d) {
+        return lastCalculatedInflationPrice
+      }
+      lastCalculatedInflationPrice = lastCalculatedInflationPrice + Math.floor(inflationInDate.v * lastCalculatedInflationPrice / 100)
+      lastFoundInflation = inflationInDate
+      return lastCalculatedInflationPrice
+    }).filter(Boolean)
+
     this.chart = new Chart(ctx, {
       type: 'line',
       data: {
         labels: history.map(({ date }) => date.slice(0, 5)),
-        datasets: [{
-          label: 'Precio',
-          data: history.map(({ price }) => price),
-          borderColor: this.$vuetify.theme.primary,
-          backgroundColor: this.$vuetify.theme.primary,
-          borderWidth: 1,
-          pointHitRadius: 17,
-          trendlineLinear: (history.length > 2) && {
-            style: '#64aa64',
-            lineStyle: 'dotted',
-            width: 2
+        datasets: [
+          {
+            label: 'Precio',
+            data: history.map(({ price }) => price),
+            borderColor: this.$vuetify.theme.primary,
+            backgroundColor: this.$vuetify.theme.primary,
+            borderWidth: 1,
+            pointHitRadius: 17,
+            trendlineLinear: (history.length > 2) && {
+              style: '#64aa64',
+              lineStyle: 'dotted',
+              width: 2
+            }
+          },
+          {
+            label: 'Esperado inflacion',
+            data: inflation,
+            borderColor: 'gray',
+            backgroundColor: 'gray',
+            borderWidth: 1,
+            pointHitRadius: 17,
           }
-        }]
+        ]
       },
       options: {
         elements: {
