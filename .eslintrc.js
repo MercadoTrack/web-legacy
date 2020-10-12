387065e6c7a4a61b350280dadf2bbaff013c4448
GMaiolo <guille.maiolo@gmail.com>
2020-10-12 16:30:18 -0300

    (feat) link to t&c in register popup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   rules: {
     'comma-dangle': 'off',
     'space-before-function-paren': 'off',
+    'no-trailing-spaces': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   globals: {
     Auth0Lock: 'readonly',
     ga: 'readonly',
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
+    parser: 'babel-eslint',
+  },
 }
