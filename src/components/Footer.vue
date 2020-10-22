f9b615d463b89f2ee966c931f4f5bbd6edd549f1
GMaiolo <guille.maiolo@gmail.com>
2020-10-22 02:38:08 -0300

    (fix) footer icons for mobile

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,42 @@
 <template>
   <v-footer app fixed class="px-4">
     <a href="https://twitter.com/MercadoTrack" target="_blank">
-      <span class="mx-2">Twitter</span>
+      <span class="mx-2" v-if="$vuetify.breakpoint.smAndUp">Twitter</span>
+      <v-btn v-else icon flat color="primary" class="ma-2 no-hover">
+        <v-icon class="social tw"></v-icon>
+      </v-btn>
     </a>
     <a href="https://www.instagram.com/mercadotrack/" target="_blank">
-      <span class="mx-2">Instagram</span>
+      <span class="mx-2" v-if="$vuetify.breakpoint.smAndUp">Instagram</span>
+      <v-btn v-else icon flat color="primary" class="ma-2 no-hover">
+        <v-icon class="social ig"></v-icon>
+      </v-btn>
     </a>
     <a href="mailto:mercadotrack@gmail.com" target="_blank">
-      <span class="mx-2">Email</span>
+      <span class="mx-2" v-if="$vuetify.breakpoint.smAndUp">Email</span>
+      <v-btn v-else icon flat color="primary" class="ma-2 no-hover">
+        <v-icon medium>mail</v-icon>
+      </v-btn>
     </a>
     <v-spacer></v-spacer>
     <router-link to="/terms" target="_blank">
-      <span class="mx-4">Términos y condiciones de uso</span>
+      <span class="mx-4" v-if="$vuetify.breakpoint.smAndUp"
+        >Términos y condiciones de uso</span
+      >
     </router-link>
     <span>MercadoTrack &copy; {{ new Date().getFullYear() }}</span>
   </v-footer>
 </template>
+
+<style lang="scss" scoped>
+.social {
+  height: 1.75rem;
+  width: 1.75rem;
+}
+.tw {
+  background-image: url('../assets/twitter-icon.svg');
+}
+.ig {
+  background-image: url('../assets/instagram-icon.svg');
+}
+</style>
